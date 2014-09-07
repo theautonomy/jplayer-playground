325d98b6ba59e0dae580751c2fdab53c9aff8bfe
Wei <weili.mail@gmail.com>
2014-09-07 19:27:11 -0400

    Add capability to move to the top of page automatically

--- web/site3/js/custom/jplayer-control.js
+++ web/site3/js/custom/jplayer-control.js
@@ -68,6 +68,7 @@ $(document).ready(function() {
       		}
       		first_track = false;
       		$(this).blur();
+      		$('html, body').animate({ scrollTop: 0 }, 'slow');
       		return false;
       	});
       
